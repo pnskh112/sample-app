c761659e79146c346a53740b6045f8c75a054244
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2021-08-15 22:54:29 +0900

    CRUD機能の構築

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon @click="toggleSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-side-icon v-show="$store.state.login_user" @click.stop="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items v-if="$store.state.login_user">
+        <v-btn @click="logout">ログアウト</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <SideNav/>
 
     <v-content>
-      <router-view></router-view>
+      <router-view/>
     </v-content>
   </v-app>
-
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import firebase from 'firebase'
 import SideNav from './components/SideNav'
+import { mapActions } from 'vuex'
 export default {
   name: 'App',
   components: {
     SideNav
+  },
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+        this.fetchAddresses()
+        if (this.$router.currentRoute.name === 'home') this.$router.push({ name: 'addresses' })
+      } else {
+        this.deleteLoginUser()
+        this.$router.push({ name: 'home' })
+      }
+    })
   },
   data () {
     return {
@@ -30,7 +45,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['toggleSideMenu'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser', 'fetchAddresses'])
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyD6hC40qQkLHBxiu4Ht-N8h090UYRAhpMw",
+  authDomain: "my-address-pj-a9ed6.firebaseapp.com",
+  projectId: "my-address-pj-a9ed6",
+  storageBucket: "my-address-pj-a9ed6.appspot.com",
+  messagingSenderId: "755203204816",
+  appId: "1:755203204816:web:e2d925de066c05b4a9e0d5",
+  measurementId: "G-FPX3PD8PXP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
